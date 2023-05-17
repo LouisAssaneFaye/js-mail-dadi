6bbea9652742fd255756cbc262557a1c1fdf39cd
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-17 16:42:30 +0200

    Esercizio mail terminato

--- Esercizio mail/js/myscript.js
+++ Esercizio mail/js/myscript.js
@@ -1,1 +1,17 @@
-const MailUtente=prompt("Type your email");
+const MailUtente = prompt("Type your email");
+const listMail = ['emaila@outlook.it','emailb@outlook.it','emailc@outlook.it','emaild@outlook.it'];
+
+let IncludedMail
+
+for (let i = 0 ; i < listMail.length ; i = i + 1 ){
+    IncludedMail = listMail[i];
+    console.log(IncludedMail);
+    if ( MailUtente === IncludedMail ) {
+        console.log('è uguale alla mail inserita da utente');
+        document.getElementById('conclusione').innerHTML = 'La tua mail rientra tra quelle registrate' ;
+    } else {
+        console.log('non è uguale alla mail inserita da utente');
+    }
+
+}
+
